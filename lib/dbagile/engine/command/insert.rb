0c26ea00142c2be8fa5da8d8695245242628ae8a
Bernard Lambeau <blambeau@gmail.com>
2010-06-10 20:52:32 +0200

    Extends the insert command to support array of tuples.

--- lib/dbagile/engine/command/insert.rb
+++ lib/dbagile/engine/command/insert.rb
@@ -8,6 +8,10 @@ class DbAgile::Engine::Command::Insert < DbAgile::Engine::Command
     argument(:TABLE_NAME, Symbol)
     argument(:TUPLE,      Hash)
   }
+  signature{
+    argument(:TABLE_NAME, Symbol)
+    argument(:TUPLES,     Array){|a| a.all?{|t| t.kind_of?(Hash)}}
+  }
         
   # Command's synopsys
   synopsis "insert a {:attribute_name => value, ...} tuple inside a table"
@@ -16,6 +20,14 @@ class DbAgile::Engine::Command::Insert < DbAgile::Engine::Command
   def execute_1(engine, table_name, tuple)
     engine.connected!
     engine.database.insert(table_name, tuple)
+  end
+        
+  # Executes the command on the engine
+  def execute_2(engine, table_name, tuples)
+    engine.connected!
+    tuples.collect do |t|
+      engine.database.insert(table_name, t)
+    end
   end
         
 end # class Quit
