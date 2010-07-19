c1dc907172dcb0e3aaff71153590a895f9a82ff0
Bernard Lambeau <blambeau@gmail.com>
2010-07-20 00:56:18 +0200

    Moves restful utils to IO helpers.

--- lib/dbagile/io.rb
+++ lib/dbagile/io.rb
@@ -6,3 +6,51 @@ require 'dbagile/io/yaml'
 require 'dbagile/io/xml'
 require 'dbagile/io/ruby'
 require 'dbagile/io/text'
+module DbAgile
+  module IO
+    
+    # Known IO formats
+    KNOWN_FORMATS = [:yaml, :csv, :json, :ruby, :text, :xml]
+    
+    # Known to_xxx IO formats
+    KNOWN_TO_FORMATS = [:yaml, :csv, :json, :ruby, :text, :xml]
+    
+    # Known from_xxx formats
+    KNOWN_FROM_FORMATS = [:yaml, :csv, :json, :ruby]
+    
+    # Which format for what extension
+    EXTENSIONS_TO_FORMAT = {
+      ".csv"   => :csv,
+      ".txt"   => :text,
+      ".json"  => :json,
+      ".yaml"  => :yaml,
+      ".yml"   => :yaml,
+      ".xml"   => :xml,
+      ".ruby"  => :ruby,
+      ".rb"    => :ruby
+    }
+    
+    # Which content type for which format
+    FORMAT_TO_CONTENT_TYPE = {
+      :csv  => "text/csv",
+      :text => "text/plain",
+      :json => "application/json",
+      :yaml => "text/yaml",
+      :xml  => "text/xml",
+      :ruby => "text/plain"
+    }
+      
+    # Checks if an extension is known. Returns the associated format.
+    def known_extension?(f)
+      f = ".#{f}" unless f[0,1] == '.'
+      EXTENSIONS_TO_FORMAT[f]
+    end
+    
+    # Checks if a format is known. Returns the associated content type.
+    def known_format?(f)
+      FORMAT_TO_CONTENT_TYPE[f]
+    end
+      
+    extend(DbAgile::IO)
+  end # module IO
+end # module DbAgile
--- lib/dbagile/restful/utils.rb
+++ lib/dbagile/restful/utils.rb
@@ -1,39 +1,7 @@
 module DbAgile
   class Restful
     module Utils
-      
-      # Which format for what extension
-      EXTENSIONS_TO_FORMAT = {
-        ".csv"   => :csv,
-        ".txt"   => :text,
-        ".json"  => :json,
-        ".yaml"  => :yaml,
-        ".yml"   => :yaml,
-        ".xml"   => :xml,
-        ".ruby"  => :ruby,
-        ".rb"    => :ruby
-      }
-      
-      # Which content type for which format
-      FORMAT_TO_CONTENT_TYPE = {
-        :csv  => "text/csv",
-        :text => "text/plain",
-        :json => "application/json",
-        :yaml => "text/yaml",
-        :xml  => "text/xml",
-        :ruby => "text/plain"
-      }
-      
-      # Checks if an extension is known. Returns the associated format.
-      def known_extension?(f)
-        f = ".#{f}" unless f[0,1] == '.'
-        EXTENSIONS_TO_FORMAT[f]
-      end
-      
-      # Checks if a format is known. Returns the associated content type.
-      def known_format?(f)
-        FORMAT_TO_CONTENT_TYPE[f]
-      end
+      include ::DbAgile::IO
       
       # Converts get parameters to a tuple for projection
       def get_to_tuple(params, heading = {})
