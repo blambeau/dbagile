84d999fe1f856fca01526a79d11123df3680471e
Bernard Lambeau <blambeau@gmail.com>
2010-07-21 14:33:15 +0200

    Adds ability to add links in documentation.

--- doc/support.rb
+++ doc/support.rb
@@ -8,6 +8,23 @@ require 'wlang/dialects/xhtml_dialect'
 require 'wlang/dialects/coderay_dialect'
 require 'wlang/dialects/standard_dialects'
 
+module DbAgile
+  module Doc
+    
+    # Converts something to a link
+    def self.to_link(txt)
+      if txt =~ /^([A-Z][a-zA-Z]+)(::[A-Z][a-zA-Z]+)*$/
+        url = "http://rdoc.info/projects/blambeau/dbagile"
+        "<a href=\"#{url}\" target=\"_blank\">#{txt}</a>"
+      else
+        puts "Warning, not a valid link: #{txt}"
+        txt
+      end
+    end
+    
+  end
+end 
+
 WLang::dialect('wdoc') do
 
   dialect("wtext", ".wtxt") do
@@ -18,6 +35,10 @@ WLang::dialect('wdoc') do
     rules WLang::RuleSet::Buffering
     rules WLang::RuleSet::Context
     rules WLang::RuleSet::XHtml
+    rule '@:' do |parser,offset|
+      text, reached = parser.parse(offset)
+      [DbAgile::Doc::to_link(text), reached]
+    end
   end
   
   dialect("wjs", ".wjs") do
