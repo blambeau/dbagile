6970e23f4c7543ee08ba6c3e3edcedbd7f676be6
Bernard Lambeau <blambeau@gmail.com>
2010-07-21 13:47:38 +0200

    Restful marker module for documentation purposes.

---
+++ lib/dbagile/restful.rb
@@ -0,0 +1,9 @@
+module DbAgile
+  #
+  # Marker module for the Restful interface.
+  #
+  # @see http://blambeau.github.com/dbagile/restful
+  #
+  module Restful
+  end # module Restful
+end # module DbAgile
