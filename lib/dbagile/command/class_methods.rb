0f0f1b7825bb81d52b65eda2061c23c121911c14
Bernard Lambeau <blambeau@gmail.com>
2010-07-23 19:19:04 +0200

    Still a bug with help --complete

--- lib/dbagile/command/class_methods.rb
+++ lib/dbagile/command/class_methods.rb
@@ -14,7 +14,7 @@ module DbAgile
       # Helper to generate command classes 
       def build_me(command_class, file)
         rdoc = DbAgile::RubyTools::rdoc_file_paragraphs(file)
-        summary, usage, description = rdoc.shift, rdoc.shift, rdoc.join
+        summary, usage, description = rdoc.shift, rdoc.shift, rdoc.join("\n")
         command_class.instance_eval{
           @summary     = summary
           @usage       = usage.gsub('#{command_name}', command_class.command_name)
